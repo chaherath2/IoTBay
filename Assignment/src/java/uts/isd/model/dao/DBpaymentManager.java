39851da81a7de3d71af43cf159d1ae3980aa1c47
ericpin1 <74949087+ericpin1@users.noreply.github.com>
2023-05-17 14:11:03 +1000

    comment DBpaymentManager.java

--- Assignment/src/java/uts/isd/model/dao/DBpaymentManager.java
+++ Assignment/src/java/uts/isd/model/dao/DBpaymentManager.java
@@ -16,7 +16,8 @@ public class DBpaymentManager {
     }
 
     /**
-     * 从表单中获取支付金额等并从绘画中获取订单对西那个创建一个支付对象并将他与订单对象关联存储至数据库
+     * Get payment amount and other details from the form, create a payment object from the order,
+     * associate it with the order object, and store it in the database.
      * @param order
      * @throws SQLException
      */
@@ -26,17 +27,17 @@ public class DBpaymentManager {
         ResultSet rs = null;
         try {
             conn = getConnection();
-            // 插入订单信息
+            // Insert order information
             stmt = conn.prepareStatement("INSERT INTO orders (customer_id, date, status) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
             stmt.setInt(1, order.getUserID());
             stmt.setDate(2, new java.sql.Date(order.getDate().getTime()));
             stmt.setString(3, order.getStatus().toString());
             stmt.executeUpdate();
-            // 获取插入的订单 ID
+            // Get the inserted order ID
             rs = stmt.getGeneratedKeys();
             if (rs.next()) {
                 int orderId = rs.getInt(1);
-                // 插入支付信息
+                // Insert payment information
                 Payment payment = order.getPayment();
                 if (payment != null) {
                     stmt = conn.prepareStatement("INSERT INTO payment (order_id, card_name, card_no, CVC, amount, date) VALUES (?, ?, ?, ?, ?, ?)");
@@ -48,7 +49,7 @@ public class DBpaymentManager {
                     stmt.setDate(6, new java.sql.Date(payment.getDate().getTime()));
                     stmt.executeUpdate();
                 }
-                // 插入订单项信息
+                // Insert order item information
                 List<OrderItem> itemList = order.getItemList();
                 if (itemList != null) {
                     stmt = conn.prepareStatement("INSERT INTO order_item (order_id, product_id, quantity, price) VALUES (?, ?, ?, ?)");
@@ -69,7 +70,7 @@ public class DBpaymentManager {
     }
 
     /**
-     * 删除订单信息
+     * Delete payment information
      * @param paymentId
      * @throws SQLException
      */
@@ -78,7 +79,7 @@ public class DBpaymentManager {
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
-            // 删除支付信息
+            // Delete payment information
             stmt = conn.prepareStatement("DELETE FROM payment WHERE payment_id = ?");
             stmt.setString(1, paymentId);
             stmt.executeUpdate();
@@ -89,7 +90,7 @@ public class DBpaymentManager {
     }
 
     /**
-     * 查询对应支付信息并返回payment对象
+     * Find corresponding payment information and return the payment object.
      * @param paymentId
      * @return
      * @throws SQLException
@@ -101,7 +102,7 @@ public class DBpaymentManager {
         Payment payment = null;
         try {
             conn = getConnection();
-            // 查询支付信息
+            // Query payment information
             stmt = conn.prepareStatement("SELECT * FROM payment WHERE payment_id = ?");
             stmt.setString(1, paymentId);
             rs = stmt.executeQuery();
@@ -123,7 +124,7 @@ public class DBpaymentManager {
     }
 
     /**
-     * 更新支付对象
+     * Update the payment object.
      * @param payment
      * @throws SQLException
      */
@@ -132,7 +133,7 @@ public class DBpaymentManager {
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
-            // 更新支付信息
+            // Update payment information
             stmt = conn.prepareStatement("UPDATE payment SET card_name = ?, card_no = ?, CVC = ?, amount = ?, date = ? WHERE payment_id = ?");
             stmt.setString(1, payment.getCardName());
             stmt.setString(2, payment.getCardNo());
@@ -148,7 +149,7 @@ public class DBpaymentManager {
     }
 
     /**
-     * 通过订单id查询对应支付信息并返回List<payment>对象
+     * Find corresponding payment information based on the order ID and return a List of Payment objects.
      * @param orderId
      * @return
      * @throws SQLException
@@ -160,7 +161,7 @@ public class DBpaymentManager {
         List<Payment> paymentList = new ArrayList<>();
         try {
             conn = getConnection();
-            // 查询支付信息
+            // Query payment information
             stmt = conn.prepareStatement("SELECT * FROM payment WHERE order_id = ?");
             stmt.setInt(1, orderId);
             rs = stmt.executeQuery();
